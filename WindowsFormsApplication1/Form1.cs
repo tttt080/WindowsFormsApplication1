f9098a8e06ff696c20efc1414ba0d3b971ccd687
小钊 刘 <tttt080@hotmail.com>
2017-08-11 22:07:40 +0800

    tttt080

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Couchbase.Configuration;
+using Couchbase;
+using Couchbase.Configuration.Client;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClientConfiguration ccf = new ClientConfiguration();
+            ccf.Servers.Add(new Uri("http://192.168.1.200:8091/pools/default"));
+
+           
+            try
+            {
+          
+            Cluster Cluster = new Cluster(ccf);
+            Cluster.OpenBucket();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            ClusterHelper.Initialize();
+            var cluster = ClusterHelper.Get();
+            using (var bucket = cluster.OpenBucket("default"))
+            {
+                var result = bucket.Insert("fookey", "foovalue");
+              //  ViewBag.InsertOne = result.Success;
+                var temp = bucket.Get<string>("fookey");
+             //   ViewBag.InsertOneResult = temp.Value;
+                result = bucket.Upsert("fookey", "foovalue2");
+             //   ViewBag.InsertTwo = result.Success;
+                var result1 = bucket.Remove("fookey");
+               // ViewBag.InsertThree = result1.Success;
+            }
+        }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+}
+    }
+}

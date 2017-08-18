b2833ea1648a791d6df2a8516de192be6d4aef57
小钊 刘 <tttt080@hotmail.com>
2017-08-18 10:28:16 +0800

    liuxiaozhao

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
+            //new
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
